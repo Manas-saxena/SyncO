27e8c50ef4214f056ea7a1e742fb2a88f730fe58
Manas Saxena <manassaxena160601@gmail.com>
2022-05-21 00:36:17 +0530

    home page work complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {Toaster} from "react-hot-toast";
 import Home from "./pages/Home";
 import EditorPage from './pages/EditorPage';
 import {BrowserRouter, Routes , Route} from 'react-router-dom'
 function App() {
   return (
     <>
+    <div>
+      <Toaster position='top-right'
+        toastOptions={{
+          success:{
+            theme:{
+              primary:'#4a2d88',
+            }
+          }
+        }}
+      />
+    </div>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home></Home>} ></Route>
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import React , {useState} from 'react'
+import {v4 as uuidv4} from "uuid";
+import taost, { toast } from 'react-hot-toast';
+const Home = () => {
+  const [roomId, setRoomId] = useState("");
+  const [userName , setUserName] = useState("");
+
+  const createNewRoom =  (e) =>{
+    e.preventDefault();
+    const id = uuidv4();
+   setRoomId(id);
+    toast.success("created a new room");
+  }
+
+  const inputChange = (e) =>{
+    if(e.target.name === "roomId")
+    { 
+      setRoomId(e.target.value);
+    }else if(e.target.name === "userName"){
+      setUserName(e.target.value);
+    }
+  }
+  
+  return (
+    <div className='homePageWrapper'>
+        <div className="formWrapper">
+          <img className='formPageLogo' src="/code-sync.png" alt="logo" />
+          <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+          <div className="inputGroup">
+            <input type="text" name="roomId" onChange={inputChange} value={roomId} placeholder='Room ID' className="inputBox" />
+            <input type="text" placeholder='USERNAME' value={userName}  onChange={inputChange} name="userName" className="inputBox" />
+            <button className='btn joinBtn'>Join</button>
+            <span className='createInfo'>
+              If you don't have an invite then create {'\u00A0'} 
+              <a onClick={createNewRoom} href="*" className='createNewBtn'>new room</a>
+            </span>
+          </div>
+        </div>
+        <footer>
+          <h4>Built with 💛 by  {'\u00A0'} <a href="https://github.com/Manas-saxena/SyncO">Manas Saxena</a> </h4>
+        </footer>
+    </div>
+  )
+}
+
+export default Home;
